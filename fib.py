4218b3fb5966bb3123ed86b205534e1288de1ca5
phouse512 <philiphouse2015@u.northwestern.edu>
2014-12-12 00:34:46 -0600

    yo

---
+++ fib.py
@@ -0,0 +1,17 @@
+hash_map = {}
+
+def fib(n):
+	key = str(n)
+	if hash_map.has_key(key):
+		print "grabbing from hash"
+		return hash_map[key]
+
+	if n < 2:
+		return n
+	else:
+		print "recursively calculating %i" % n
+		val = fib(n-2) + fib(n-1)
+		hash_map[key] = val
+		return val
+
+print fib(10)
---
+++ stock_sell.py
@@ -0,0 +1,6 @@
+def stock_price(array):
+	current_min = array[0]
+	current_max = 0
+
+	for price in array:
+		print price 
